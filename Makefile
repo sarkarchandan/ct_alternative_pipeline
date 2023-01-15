a9acbb497651dc7414b2301d8ed43b8db462ccae
Chandan Sarkar <sarkar1986.chandan@gmail.com>
2023-01-15 16:38:08 +0100

    chore: Established server for reconstructor

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 setup:
 	cp ./framework/templates/base.html ./producer/templates/
 	cp ./framework/templates/base.html ./reconstructor/templates/
-	cp ./framework/static/css/styles.css ./producer/static/css
-	cp ./framework/static/css/styles.css ./reconstructor/static/css
+	cp ./framework/static/css/styles.css ./producer/static/css/
+	cp ./framework/static/css/styles.css ./reconstructor/static/css/
 	docker-compose up
 gen:
 	python producer/server.py
--- reconstructor/server.py
+++ reconstructor/server.py
@@ -1,8 +1,27 @@
+from pathlib import Path
+import sys
 import time
 import json
+from threading import Thread
+
+from flask import Flask, render_template
 from kafka import KafkaConsumer
 
-if __name__ == '__main__':
+framework_path: str = Path().absolute().__str__()
+print(f"Abs Path: {framework_path}")
+sys.path.append(framework_path)
+
+from framework.recon import Reconstructor
+
+# Initializes container process
+app: Flask = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index() -> str:
+    """Defines route for the application"""
+    return render_template("index.html")
+
+def subscribe_for_image_data() -> None:
     consumer: KafkaConsumer = KafkaConsumer(
         'image_data',
         bootstrap_servers=['localhost:9092'],
@@ -15,3 +34,8 @@ if __name__ == '__main__':
         event_data = events.value
         print(f'Received: {event_data}')
         time.sleep(2)
+
+if __name__ == '__main__':
+    t: Thread = Thread(target=subscribe_for_image_data, args=())
+    t.start()
+    app.run(host="0.0.0.0", port=9992)
