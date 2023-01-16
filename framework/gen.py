0f58955bf5371582351292a84439acddc502725e
Chandan Sarkar <sarkar1986.chandan@gmail.com>
2023-01-16 22:45:27 +0100

    chore: Update notebook with projections

--- framework/gen.py
+++ framework/gen.py
@@ -10,6 +10,7 @@ from typing import Generator, List, Dict, Any, Tuple
 from dataclasses import dataclass
 import yaml
 import numpy as np
+import matplotlib.pyplot as plt
 from skimage.transform import rotate
 
 @dataclass
@@ -23,6 +24,7 @@ class DataGenerator:
 
     @classmethod
     def from_config(cls, config: str = "config.yaml") -> DataGenerator:
+        """Instantiates abstract dataset generator from config"""
         cfg: Dict[str, Any]
         with open(file=config, mode="r") as stream:
             cfg = yaml.safe_load(stream=stream)
@@ -30,23 +32,42 @@ class DataGenerator:
 
     @property
     def abstract_obj_image(self) -> np.ndarray:
-        """Computes base object image"""
+        """Creates abstract object image"""
         img: np.ndarray = np.ones(shape=(self.obj_dim, self.obj_dim))
         diag_len: int = len(np.diag(img) // 2)
         pad_img: np.ndarray = np.pad(
             array=img, 
             pad_width=diag_len + self.padding
         )
-        radial_values: np.ndarray = np.linspace(
-            start=-1, 
-            stop=1, 
-            num=pad_img.shape[0])
-        xv, yv = np.meshgrid(radial_values, radial_values)
+        lsp: np.ndarray = np.linspace(start=-1, stop=1, num=pad_img.shape[0])
+        xv, yv = np.meshgrid(lsp, lsp)
         pad_img[(xv - 0.1)**2 + (yv - 0.2)**2 < 0.01] = 2
         return pad_img
 
+    def plot_samples(self, 
+        num_samples: int = 5, 
+        figsize: Tuple[int, int] = (15, 15)) -> None:
+        """Plots a subsection of the abstract object dataset
+        Args:
+            num_samples: Number of samples to be plotted
+            figsize: Figure size for the plot
+        """
+        samples: List[np.ndarray] = list(self())
+        _, axes = plt.subplots(nrows=1, ncols=num_samples, figsize=figsize)
+        for idx in range(num_samples):
+            axes[idx].imshow(samples[idx]);
+            axes[idx].axis('off');
+
+    def to_array(self) -> np.ndarray:
+        """Convenient method to yield the dataset as an ndarray"""
+        return np.array(list(self()))
+
+    def create_sinogram(self) -> np.ndarray:
+        pass
+
     @property
     def dataset_shape(self) -> Tuple[int, int, int]:
+        """Convenient method to yield the shape of the dataset"""
         padded_dim: int = (2 * self.obj_dim) + self.obj_dim + (2 * 10)
         length: int = np.arange(
             start=self.angle_start, 
@@ -55,6 +76,7 @@ class DataGenerator:
         return (length, padded_dim, padded_dim)
 
     def __call__(self) -> Generator[np.ndarray, None, None]:
+        """Yields abstract dataset as a generator expression"""
         angles: np.ndarray = np.arange(
             start=self.angle_start, 
             stop=self.angle_end, 
--- framework/recon.py
+++ framework/recon.py
@@ -4,8 +4,49 @@
     Encapsulates utilities for reconstruction of abstract 2D object
 """
 
+from typing import List, Tuple
+import numpy as np
+import matplotlib.pyplot as plt
+
 class Reconstructor:
-    pass
+
+    dataset: List[np.ndarray]
+    rotation_profile: Tuple[int, int, int]
+    
+    def __init__(self, 
+        dataset: List[np.ndarray], 
+        rotation_profile: Tuple[int, int, int]) -> None:
+        self.dataset = dataset
+        self.rotation_profile = rotation_profile
+
+    @property
+    def _linear_space(self) -> np.ndarray:
+        """Convenient function to compute linear space across the object. This 
+        would be required to compute the differential over linear space"""
+        assert(len(self.dataset) > 0)
+        return np.linspace(start=-1, stop=1, num=self.dataset[0].shape[0])
+
+    def create_sinogram(self) -> np.ndarray:
+        """Computes line integrals across all rotated samples from the 
+        abstract object dataset"""
+        diff_space: np.ndarray = np.diff(self._linear_space)[0]
+        return np.array(
+            [rotation.sum(axis=0) * diff_space for rotation in self.dataset])
+
+    def plot_projections(self, 
+        num_samples: int = 5, 
+        figsize: Tuple[int, int] = (15, 2)) -> None:
+        angles: np.ndarray = np.arange(
+            start=self.rotation_profile[0], 
+            stop=self.rotation_profile[1], 
+            step=self.rotation_profile[2])[:num_samples]
+        sgs: np.ndarray = self.create_sinogram()
+        _, axes = plt.subplots(nrows=1, ncols=len(angles), figsize=figsize)
+        for idx in range(len(angles)):
+            axes[idx].plot(self._linear_space, sgs[idx, :]);
+            axes[idx].set_xlabel(f'Angle: {angles[idx]}')
+            axes[idx].set_ylabel('$\ln({I_0}/I)$')
+
 
 if __name__ == "__main__":
     pass
